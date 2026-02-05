0eaa063328312c2fd20916d914c3a61cd38def7c
Elton tanyumara <24b11cs525@adityauniversity.in>
2026-02-05 13:35:17 +0530

    Add files via upload

---
+++ prime.java
@@ -0,0 +1,22 @@
+import java.io.*;
+class prime{
+    public static void main(String args[])
+    {
+        int i,j,count;
+        for(i=2;i<=100;i++){
+            count =0;
+            for (j=1;j<=i;j++){
+
+                if (i%j==0)
+                {
+                    count++;
+                }
+            }
+            if (count ==2){
+            System.out.println(i+);
+            }
+
+
+        }
+    }
+}
